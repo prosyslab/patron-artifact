571e78c6d95faf46c817ef531f5f593f794b3857
spearo2 <leechanggong@gmail.com>
2024-04-27 14:29:21 +0900

    [patron] fix multiprocessing

--- bin/patron.py
+++ bin/patron.py
@@ -8,6 +8,7 @@ import csv
 import datetime
 from threading import Thread
 from time import sleep
+import time
 
 expriment_ready_to_go = {
     "patron": [
@@ -16,51 +17,76 @@ expriment_ready_to_go = {
     ],
     "patchweave": [
         "1", "2-1", "3", "4", "6", "7", "8", "9-1", "10",
-        "12-1", "13", "17", "19", "21", "22", "23", "24"
+        "12-1", "13", "17", "19", "21", "22", "24"
     ]
     }
 level = ""
 donor_list = []
-jobs_finished = False
-current_job = ""
+jobs_finished = []
 
-def run_patron(worklist):
-    global current_job
-    tsv_file = open(os.path.join(config.configuration["OUT_DIR"], "patron_{}.tsv".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))), 'a')
-    writer = csv.writer(tsv_file, delimiter='\t')
-    writer.writerow(["PROJECT", "ALARM_ID", "STATUS"])
-    tsv_file.flush()
+def manage_patch_status(out_dir, current_job, job_cnt):
+    global jobs_finished
+    log(INFO, "Status Manager is Running!")
+    with open(os.path.join(out_dir, "status.tsv"), 'a') as f:
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerow(["Donee Name", "Donor Benchmark", "Donor #", "Donee #", "Pattern Type","Correct?", "Diff"])
+        f.flush()
+        while current_job == "" and not jobs_finished[job_cnt]:
+                time.sleep(10)
+        dir_cache = os.listdir(out_dir)
+        while not jobs_finished[job_cnt]:
+            if len(dir_cache) != len(os.listdir(out_dir)):
+                new_files = set(os.listdir(out_dir)) - set(dir_cache)
+                for file in new_files:
+                    if file.endswith('.patch'):
+                        diff = ""
+                        while diff == "":
+                            with open(os.path.join(out_dir, file), 'r') as df:
+                                diff = df.read()
+                                if diff == "" and not jobs_finished[job_cnt]:
+                                    continue
+                                else:
+                                    file_parsed = file.split('.')[0].split('_')
+                                    donee_num = file_parsed[1].strip()
+                                    for infof in os.listdir(out_dir):
+                                        if infof.endswith('.c') and donee_num in infof:
+                                            parsed_info = infof.split('_')[1:]
+                                            tmp_list = parsed_info[0].split('-')
+                                            benchmark = tmp_list[0].strip()
+                                            donor_num = tmp_list[1].strip()
+                                            pattern = "ALT" if parsed_info[-1].strip() == "1" else "NORMAL"
+                                            writer.writerow([current_job, benchmark, donor_num, donee_num, pattern, "-", diff])
+                                            f.flush()
+                                    break
+            else:
+                time.sleep(10)
+
+def run_patron(cmd, job_cnt):
     os.chdir(config.configuration["PATRON_ROOT_PATH"])
-    process_cnt = 0
-    for cmd in worklist:
-        current_job = cmd[2]
-        log(INFO, f"Running patron with {cmd}")
-        result = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        process_cnt += 1
-        if process_cnt <= config.configuration["ARGS"].process:
-            result.wait()
-            process_cnt -= 1
-        if result.returncode != 0:
-            log(ERROR, f"Failed to run patron with {cmd}")
-            log(ERROR, result.stderr.read().decode('utf-8'))
-            writer.writerow([cmd[1].split('/')[-1], cmd[2], "X"])
-            tsv_file.flush()
-        else:
-            log(INFO, f"Successfully ran patron with {cmd}")
-            writer.writerow([cmd[1].split('/')[-1], cmd[2], "O"])
-            tsv_file.flush()
+    current_job = os.path.basename(cmd[2])
+    sub_out_dir = cmd[-1]
+    status_manager = Thread(target=manage_patch_status, args=(sub_out_dir, current_job, job_cnt))
+    status_manager.start()
+    log(INFO, f"Running patron with {cmd}")
+    return status_manager, subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
 
 def mk_worklist():
     base_cmd = [config.configuration["PATRON_BIN_PATH"]]
     worklist = []
+    cnt = 0
     for donee in config.configuration["DONEE_LIST"]:
         package = donee.split('/')[-1]
         sub_out = os.path.join(config.configuration["OUT_DIR"], package)
+        db_opt = ["--db", os.path.join(config.configuration["DB_PATH"])]
         if not os.path.exists(sub_out):
             os.mkdir(sub_out)
+        else:
+            os.mkdir(sub_out + "_" + str(cnt))
+            sub_out = sub_out + "_" + str(cnt)
         out_opt = ["-o", sub_out]
-        worklist.append(base_cmd + ['patch', donee] + out_opt)
+        worklist.append(base_cmd + ['patch', donee] + db_opt + out_opt)
+        cnt += 1
     return worklist
 
 def run_sparrow():
@@ -168,47 +194,28 @@ def check_database():
 def construct_database():
     check_sparrow()
     run_sparrow()
-    mk_database()
+    mk_database()                      
 
-def manage_patch_status():
+def collect_job_results(PROCS, work_cnt):
     global jobs_finished
-    global current_job
-    log(INFO, "Status Manager is Running!")
-    with open(os.path.join(config.configuration["OUT_DIR"], "status.tsv"), 'a') as f:
-        writer = csv.writer(f, delimiter='\t')
-        writer.writerow(["Donee Name", "Donor Benchmark", "Donor #", "Donee #", "Pattern Type","Correct?", "Diff"])
-        f.flush()
-        while not jobs_finished:
-            if current_job == "":
-                time.sleep(10)
-                continue
-            dir_cache = os.listdir(config.configuration["OUT_DIR"])
-            time.sleep(10)
-            if dir_cache != os.listdir(config.configuration["OUT_DIR"]):
-                new_files = set(os.listdir(config.configuration["OUT_DIR"])) - set(dir_cache)
-                for file in new_files:
-                    if file.endswith('.patch'):
-                        with open(os.path.join(config.configuration["OUT_DIR"], file), 'r') as df:
-                            diff = df.read()
-                            if diff == "":
-                                continue
-                            else:
-                                file_parsed = file.split('.')[0].split('_')
-                                donee_num = file_parsed[1].strip()
-                                for infof in os.listdir(config.configuration["OUT_DIR"]):
-                                    if infof.endswith('.c') and donee_num in infof:
-                                        parsed_info = infof.split('_')[1:]
-                                        tmp_list = parsed_info[0].split('-')
-                                        benchmark = tmp_list[0].strip()
-                                        donor_num = tmp_list[1].strip()
-                                        pattern = "ALT" if parsed_info[-1].strip() == "1" else "NORMAL"
-                                        writer.writerow([current_job, benchmark, donor_num, donee_num, pattern, "-", diff])
-                                        f.flush()
-                                        
- 
+    for j in range(len(PROCS)):
+        cmd, work_id, m, proc = PROCS[j]
+        if proc.poll() is not None:
+            jobs_finished[work_id] = True
+            m.join()
+            work_cnt -= 1
+            if proc.returncode != 0:
+                log(ERROR, f"Failed to run patron with {cmd}")
+                log(ERROR, proc.stderr.read().decode('utf-8'))
+            else:
+                log(INFO, f"Successfully ran patron with {cmd}")
+            PROCS.pop(j)
+            break
+    return PROCS, work_cnt
+
 def main():
-    global jobs_finished
     global level
+    global jobs_finished
     level = "PATRON"
     config.setup(level)
     if config.configuration["DATABASE_ONLY"]:
@@ -221,11 +228,26 @@ def main():
     if not check_donee(config.configuration["DONEE_LIST"]):
         log(ERROR, "DONEE is not ready.")
         exit(1)
-    status_manager = Thread(target=status_manager, args=())
-    status_manager.start()
-    run_patron(mk_worklist())
-    jobs_finished = True
-    status_manager.join()
+    worklist = mk_worklist()
+    work_cnt = 0
+    PROCS = []
+    for i in range(len(worklist)):
+        jobs_finished.append(False)
+        log(INFO, f"Worklist: {worklist[i]}")
+        manager, p = run_patron(worklist[i], i)
+        PROCS.append((worklist[i], i, manager, p))
+        time.sleep(5)
+        work_cnt += 1
+        if work_cnt >= config.configuration["ARGS"].process:
+            log(WARNING, "Waiting for the current jobs to finish...")
+        while work_cnt >= config.configuration["ARGS"].process:
+            PROCS, work_cnt = collect_job_results(PROCS, work_cnt)
+            time.sleep(5)
+    while PROCS != []:
+        PROCS, work_cnt = collect_job_results(PROCS, work_cnt)
+        time.sleep(5)
+
+
     log(INFO, "All jobs are finished.")
     log(INFO, "Please check the status.tsv file for the results.")
     
