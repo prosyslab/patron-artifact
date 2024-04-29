6cfe8d6ef0030989aee9c254ddc0eea31c1bc358
spearo2 <leechanggong@gmail.com>
2024-04-29 11:28:46 +0900

    fix typo

--- bin/config.py
+++ bin/config.py
@@ -210,7 +210,7 @@ def setup(level):
         if configuration["ARGS"].database:
             configuration["DATABASE_ONLY"] = True
         else:
-            log("INFO", "Configuring target donee files under given directories {}".format(configuration["ARGS"].donee))
+            logger.log("INFO", "Configuring target donee files under given directories {}".format(configuration["ARGS"].donee))
             target_dirs = [ os.path.abspath(don) for don in configuration["ARGS"].donee ]
             donee_list = []
             for target in target_dirs:
@@ -222,5 +222,5 @@ def setup(level):
                         if file.endswith(".c"):
                             donee_list.append(os.path.dirname(os.path.abspath(os.path.join(root, file))), os.path.abspath(os.path.join(root, file)))
             configuration["DONEE_LIST"] = donee_list
-            log("INFO", "Configured donee files: {}".os.path.basename(format(configuration["DONEE_LIST"])))
+            logger.log("INFO", "Configured donee files: {}".os.path.basename(format(configuration["DONEE_LIST"])))
     logger.log(logger.INFO, "Configuration: {}".format(configuration))
