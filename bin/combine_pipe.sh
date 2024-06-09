d7da117fd8818e9e0082403e78af98d819eb0bfb
spearo2 <leechanggong@gmail.com>
2024-06-09 19:56:44 +0900

    [combine] handle more syntactic exceptions

--- bin/combine_pipe.sh
+++ bin/combine_pipe.sh
@@ -2,13 +2,21 @@ remove_error_prone_i_files() {
     for file in *.i; do sed -i 's/\bconst\b//g' $file; done
     for file in *.i; do sed -i 's/(int (char \* ))/(char \*)/g' "$file"; done
     for file in *.i; do sed -i '/_Float/,/;/d' "$file"; done
-    for file in *.i; do sed -i 's/"[^"]*"/"-"/g' "$file"; done
+    for file in *.i; do sed -i '/#line [1-9][0-9]\{0,7\}/!{/# [1-9][0-9]\{0,7\}/!s/"\([^"\\]*\|\\.\)*"/"-"/g}' "$file"; done
+}
+
+remove_error_prone_c_files() {
+    for file in *.c; do sed -i 's/\bconst\b//g' $file; done
+    for file in *.c; do sed -i 's/(int (char \* ))/(char \*)/g' "$file"; done
+    for file in *.c; do sed -i '/strfromf32/,/;/d' $file; done
+    for file in *.c; do sed -i '/_Float/,/;/d' "$file"; done
+    for file in *.i; do sed -i '/#line [1-9][0-9]\{0,7\}/!{/# [1-9][0-9]\{0,7\}/!s/"\([^"\\]*\|\\.\)*"/"-"/g}' "$file"; done
 }
 
 parse () {
     remove_error_prone_i_files
     sparrow -il -frontend claml *.i > $1
-    remove_error_prone_i_files
+    remove_error_prone_c_files
 }
 
 com () {
