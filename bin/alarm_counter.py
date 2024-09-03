f6742ba5d4df25ba6e90e91732df2d9fbeb7347e
spearo2 <leechanggong@gmail.com>
2024-09-03 13:04:53 +0900

    [script]fix typo

--- bin/alarm_counter.py
+++ bin/alarm_counter.py
@@ -11,24 +11,24 @@ fp = open(os.path.join(OUT_DIR, out_name), 'w')
 def run(target_dir):
   for root, dirs, files in os.walk(target_dir):
     for d in dirs:
-      if d == 'sparrow-out'
-        project_path = root
-        alarm_path = os.path.join(project_path, 'sparrow-out', 'taint', 'datalog')
-        cnt = 0
-        for alarm in os.listdir(alarm_path):
-          if alarm == Alarm.map
-            continue
-          cnt += 1
-        fp.write(f"{project_path}\t{cnt}\n")
-        fp.flush()
-        print(f"{project_path}\t{cnt}")
+        if d == 'sparrow-out':
+            project_path = root
+            alarm_path = os.path.join(project_path, 'sparrow-out', 'taint', 'datalog')
+            cnt = 0
+            for alarm in os.listdir(alarm_path):
+                if alarm == 'Alarm.map':
+                    continue
+            cnt += 1
+            fp.write(f"{project_path}\t{cnt}\n")
+            fp.flush()
+            print(f"{project_path}\t{cnt}")
         
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python alarm_counter.py <target_dir>")
         sys.exit(1)
 
-  run(sys.argv[1])
+    run(sys.argv[1])
     
     
 fp.close()
