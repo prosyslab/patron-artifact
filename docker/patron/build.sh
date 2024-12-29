759007f4b22f3d22c60e8c18a06a0f9db0483757
zackcglee <leechanggong@gmail.com>
2024-12-30 04:13:21 +0900

    [docker] update Dockerfile

--- docker/patron/build.sh
+++ docker/patron/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -eux
+SRC=/src
+mkdir -p $SRC
+cd $SRC; git clone https://github.com/prosyslab/patron-artifact.git --recursive
 
-docker build -t prosyslab/patron-artifact .
+pip install -r $SRC/patron-artifact/requirements.txt
+chmod +x $SRC/patron-artifact/build.sh
+$SRC/patron-artifact/build.sh
